4cc01b2750e8c271a9f0aa5cd781ca0bf910e0ea
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-10-26 14:06:35 +0300

    added: import and export commands

--- surrealdb_helper/main.py
+++ surrealdb_helper/main.py
@@ -1,4 +1,6 @@
 import modules
+import argparse
+import sys
 
 def get_config():
     import json
@@ -10,7 +12,22 @@ def get_config():
 
 def run():
     config = get_config()
-    modules.export_db(config)
+    parser = argparse.ArgumentParser(description='Helper script to help you import and export project database')
+    subparsers = parser.add_subparsers()
+
+    export_parser = subparsers.add_parser('export', help="Export database to SQL file")
+    export_parser.add_argument('file_name', help="File name to be exported", nargs="?", default="export.db")
+    export_parser.set_defaults(func=modules.export_db)
+
+    import_parser = subparsers.add_parser('import', help="Import SQL file to database")
+    import_parser.add_argument('file_name', help="File name to be imported", nargs="?", default="export.db")
+    import_parser.set_defaults(func=modules.export_db)
+    
+    args = parser.parse_args()
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    args.func(args.file_name, config)
     
 
 
--- surrealdb_helper/modules.py
+++ surrealdb_helper/modules.py
@@ -1,5 +1,19 @@
 import subprocess
 
-def export_db(config):
-    arguments = ["surreal", "export", "--conn", f"{config['url']}", "--user", f"{config['username']}", "--pass", f"{config['password']}", "--ns", f"{config['namespace']}", "--db", f"{config['database']}", "export.sql"]
+def export_db(file, config):
+    arguments = ["surreal", "export", 
+                 "--conn", f"{config['url']}", 
+                 "--user", f"{config['username']}", 
+                 "--pass", f"{config['password']}", 
+                 "--ns", f"{config['namespace']}", 
+                 "--db", f"{config['database']}", file]
+    subprocess.run(arguments)
+
+def inport_db(file, config):
+    arguments = ["surreal", "import", 
+                 "--conn", f"{config['url']}", 
+                 "--user", f"{config['username']}", 
+                 "--pass", f"{config['password']}", 
+                 "--ns", f"{config['namespace']}", 
+                 "--db", f"{config['database']}", file]
     subprocess.run(arguments)
